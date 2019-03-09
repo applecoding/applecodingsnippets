880f5ab0ba55239b1f506a812ccf8e664cb19960
Julio César Fernández Muñoz <juliocesar.fernandez@gabhel.com>
2019-03-09 20:24:55 +0100

    Código terminado

--- Snippet #1 Result Type Swift 5.playground/Pages/Enums de carga.xcplaygroundpage/Contents.swift
+++ Snippet #1 Result Type Swift 5.playground/Pages/Enums de carga.xcplaygroundpage/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var str = "Hello, playground"
+enum Resultado<Int, String> {
+    case OK(Int)
+    case ERROR(String)
+}
+
+let valor:Resultado<Int,String> = .OK(10)
+let otro:Resultado<Int,String> = .ERROR("Ha dado error")
+
+func sumaNumeros(nums:[Int]) -> Resultado<Int,String> {
+    if nums.isEmpty {
+        return .ERROR("No has mandado nada")
+    }
+    return .OK(nums.reduce(0,+))
+}
+
+let numeros = [1,4,5,6,4,5,4,5,4,7,8,9,4]
+let suma = sumaNumeros(nums: numeros)
+let suma2 = sumaNumeros(nums: [])
+
+if case .OK(let sumatorio) = suma {
+    print("Resultado \(sumatorio)")
+}
+
+switch suma2 {
+case .OK(let resultado): print("Resultado \(resultado)")
+case .ERROR(let error): print("Ha ocurrido un error \(error)")
+}
+
--- Snippet #1 Result Type Swift 5.playground/Pages/Result Type.xcplaygroundpage/Contents.swift
+++ Snippet #1 Result Type Swift 5.playground/Pages/Result Type.xcplaygroundpage/Contents.swift
@@ -1,7 +1,44 @@
-//: [Previous](@previous)
+import UIKit
 
-import Foundation
+enum ErrorSuma:Error {
+    case arrayVacio
+}
 
-var str = "Hello, playground"
+func sumaNumeros(nums:[Int]) -> Result<Int, ErrorSuma> {
+    if nums.isEmpty {
+        return .failure(.arrayVacio)
+    }
+    return .success(nums.reduce(0,+))
+}
 
-//: [Next](@next)
+let numeros = [1,4,5,6,4,5,4,5,4,7,8,9,4]
+let suma = sumaNumeros(nums: numeros)
+let suma2 = sumaNumeros(nums: [])
+
+if case .success(let sumatorio) = suma {
+    print("Resultado \(sumatorio)")
+}
+
+switch suma2 {
+case .success(let resultado): print("Resultado \(resultado)")
+case .failure(let error): print("Ha ocurrido un error \(error)")
+}
+
+if let suma = try? suma.get() {
+    print("El resultado de la suma \(suma)")
+}
+
+do {
+    let sumaOK = try suma2.get()
+    print(sumaOK)
+} catch ErrorSuma.arrayVacio {
+    print("El array está vacío")
+}
+
+if let fichero = Bundle.main.url(forResource: "test", withExtension: "json") {
+    let result = Result { try Data(contentsOf: fichero) }
+    if case .success(let contenido) = result {
+        print(String(data: contenido, encoding: .utf8)!)
+    }
+}
+
